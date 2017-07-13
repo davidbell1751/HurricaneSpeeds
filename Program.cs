77388b3bb691179b1a473f9c54df7f92bbef98f0
David <davidcreaghbell@gmail.com>
2017-07-13 15:44:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+class Adminssion
+{
+
+    static void Main()
+    {
+        string numberString;
+            int speed;
+
+        Write("Enter wind speed ");
+        numberString = ReadLine();
+        speed = Convert.ToInt32(numberString);
+
+        if (speed >= 157)
+            WriteLine("CLass 5 Hurricane");
+        else
+            if
+            (speed >= 130)
+            WriteLine("Class 4 Hurricane");
+        else
+            if
+            (speed >= 111)
+            WriteLine("Class 3 Hurricane");
+        else
+            if
+            (speed >= 96)
+            WriteLine("Class 2 Hurricane");
+        else
+            if
+            (speed >= 74)
+            WriteLine("Class 1 Hurricane");
+        else
+            WriteLine("Not a Hurricane");
+    }
+}
